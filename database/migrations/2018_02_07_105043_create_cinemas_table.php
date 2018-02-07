1410550e69244ed5c7b4ad35465de280a1253419
Brendan Kelly <brendan@pion.com.au>
2018-02-07 22:12:22 +1100

    added models and migrations for the cinemas, movies and sessions db tables

---
+++ database/migrations/2018_02_07_105043_create_cinemas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCinemasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cinemas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('name');
+            $table->text('address');
+            $table->text('url');
+            $table->text('phone');
+            $table->decimal('latitude', 8, 6);
+            $table->decimal('longitude', 9, 6);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cinemas');
+    }
+}
